dc6bbd13b00bd1b1c83fcb6d7b5e9bec93ef7992
mveyone <mveyone@outlook.fr>
2023-01-20 12:40:04 +0100

    terraform ingress

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,17 +1,17 @@
 resource "aws_security_group" "UbuntuSG" {
 
   ingress {
-    from_port = 22
-    to_port   = 22
+    from_port = 0
+    to_port   = 65535
     protocol  = "tcp"
     cidr_blocks  = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
-    cidr_blocks  = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port = 8080
+  #   to_port   = 8080
+  #   protocol  = "tcp"
+  #   cidr_blocks  = ["0.0.0.0/0"]
+  # }
 
   egress  {
     from_port = 0
